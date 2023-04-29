bb028b87c8bae03f787d881fa58f9b024f315fd4
ih_____________eb <58560500+ihebXxxjaouadi@users.noreply.github.com>
2023-04-29 02:40:43 +0100

    Add files via upload

---
+++ wathek/dto/ReclamationDTO.java
@@ -0,0 +1,14 @@
+package com.wathek.wathek.dto;
+
+import javax.validation.constraints.NotNull;
+
+public class ReclamationDTO {
+
+
+
+    @NotNull
+    private Long idProduit;
+    private String description;
+
+
+}
